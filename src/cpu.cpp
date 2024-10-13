3a41d9db0f183976795fec80fc59e8214b464df5
Linuxperoxo <linuxperoxo@gmail.com>
2024-10-12 22:05:46 -0300

    add ADDRSMODE and REGCODE in cpu src

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -6,7 +6,7 @@
  *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
  *    |  AUTHOR    : Linuxperoxo                   |
  *    |  FILE      : cpu.cpp                       |
- *    |  SRC MOD   : 11/10/2024                    |
+ *    |  SRC MOD   : 12/10/2024                    |
  *    |                                            |
  *    O--------------------------------------------/
  *
@@ -16,39 +16,46 @@
 #include <cctype>
 #include <cstdint>
 #include <cstdlib>
-#include <ios>
 #include <iostream>
 
 #include "../include/cpu/cpu.hpp"
 #include "../include/bus/bus.hpp"
 
-#define FIRST_ADDRS_TO_READ_INSTRUCTION 0xFFFE
-#define FIRST_ADDRS_STACK_PTR 0xFF
+#define FIRST_ADDRS_TO_READ_INSTRUCTION 0xFFFD
 #define FIRST_INSTRUCTION_OPCODE 0x00
+
+#define SEC_ADDRS_TO_READ_INSTRUCTION 0xFFFE
 #define SEC_INSTRUCTION_OPCODE 0x01
+
+#define FIRST_ADDRS_STACK_PTR 0xFF
 #define ROM_INIT 0x8000
-#define ZERO 0x00
 
 constexpr uint16_t CLOCK_FREC {1000000000 / 1790000 };
 
 CPU::CPU(BUS* _bus_to_link) noexcept
 {
   linkbus(_bus_to_link);
-  
-  _PC = FIRST_ADDRS_TO_READ_INSTRUCTION;
-  _F  = ROM_INIT;
 
-  // std::cout << std::hex << static_cast<int>(_PC) << '\n';
-  
-  write(_PC, FIRST_INSTRUCTION_OPCODE);
-  run();
+  /*
+   *
+   * Os primeiros 2 ciclos de inicialização
+   *
+   */
+   
+  write(FIRST_ADDRS_TO_READ_INSTRUCTION, FIRST_INSTRUCTION_OPCODE); // Gravando a primeira instrução que é um RST
+  run(); // Primeiro ciclo
 
-  write(_PC, SEC_INSTRUCTION_OPCODE);
-  run();
-
-  // std::cout << std::hex << static_cast<int>(_PC) << '\n';
+  write(SEC_ADDRS_TO_READ_INSTRUCTION, SEC_INSTRUCTION_OPCODE); // Gravando a segundo instrução que é JMP para o início da ROM
   
-  // (this->*_opcode[0]._instruct_ptr)(&_opcode[0]);  
+  /*
+   *
+   * Como o JMP lê os próximos 2 bytes para saber qual endereço ele deve pular, então escrevemos nesse local, o endereço 
+   * que vamos querer ir, no caso 0x8000
+   *
+   */
+
+  write(SEC_ADDRS_TO_READ_INSTRUCTION + 1, 0x80); 
+  run(); // Segundo ciclo
 }
 
 /*
@@ -60,6 +67,16 @@ CPU::CPU(BUS* _bus_to_link) noexcept
  * NONE            -> void
  *
  */
+
+NONE CPU::sts() noexcept
+{
+  std::cout << "INSTRUCTION : \"" << _opcode[_X]._name << "\" "
+            << "PC ADDRS: \"0x" << std::hex << _PC << "\" "
+            << "STACK ADDRS: \"0x" << std::hex << static_cast<int>(_STKPTR) << "\"" 
+            << '\n';
+
+  std::cout << std::dec;
+}
 
 NONE CPU::linkbus(BUS* _bus) noexcept
 {
@@ -78,64 +95,89 @@ NONE CPU::read(ADDRS_BITS_SIZE _data_to_read) noexcept
 
 NONE CPU::run() noexcept
 {
-  read(_PC); // Lendo a memória que o PC está apontando, essa  memória será armazenada no registrador _X
+  read(_PC); // Lendo a memória que o registrador PC está apontando, essa  memória será armazenada no registrador _X
 
-  std::cout << "INSTRUCTION : \"" << _opcode[_X]._name << "\" "
-            << "SIZE : \"" << _opcode[_X]._bytes_to_read + 1 << " Bytes\" "
-            << "PC ADDRS: \"0x" << std::hex << _PC << "\" "
-            << "STACK ADDRS: \"0x" << std::hex << _STKPTR << "\"" 
-            << '\n';
-
-  std::cout << std::dec;
+  sts(); // Mostrando informações de execução
 
   (this->*_opcode[_X]._instruct_ptr)(&_opcode[_X]); // Executando a instrução
 }
 
 NONE CPU::RST(CPU::INSTRUCTION*) noexcept
 {
+  /*
+   *
+   * Resetando tudo para o estado inicial
+   *
+   */
+
   _A      = 0x00;
   _X      = 0x00;
   _Y      = 0x00;
-  _F      = ROM_INIT;
-  _PC     = FIRST_ADDRS_TO_READ_INSTRUCTION; 
+  _F      = 0x00;
+  _H      = 0x00;
   _STKPTR = FIRST_ADDRS_STACK_PTR;
-  _STATUS = 0x00;
+  _PC     = FIRST_ADDRS_TO_READ_INSTRUCTION; 
   
-  ++_PC;
+  _STATUS = 0x00;
+
+  ++_PC; // Isso é temporário
 }
+
+/*
+ *
+ * ADDRESS MODE:
+ *
+ */
+
+NONE CPU::ADDRS8B() noexcept
+{
+  read(_PC + 1);
+  _Y = _X;
+}
+
+/*
+ *
+ * Apenas uma pequena gambiarra para ler um endereço de memória de 16 bits
+ *
+ */
+
+NONE CPU::ADDRS16B() noexcept
+{
+  read(_PC + 1);
+
+  _Y = _X;
+
+  read(_PC + 2);
+
+  _F = _X;
+}
+
+/*
+ *
+ *  INSTRUCTIONS
+ *
+ */
 
 NONE CPU::ADD(CPU::INSTRUCTION*) noexcept
 {
   std::cout << "ADD\n";
+  ++_PC;
 }
 
 NONE CPU::SUB(CPU::INSTRUCTION*) noexcept
 {
   std::cout << "SUB\n";
+  ++_PC;
 }
 
-NONE CPU::JMP(CPU::INSTRUCTION*) noexcept
+NONE CPU::JMP(CPU::INSTRUCTION* _instruct) noexcept
 {
-  _PC = _F; 
-}
+  (this->*_instruct->_addrs_mode)();
+  
+  _PC = _PC << 16;
 
-NONE CPU::PRT(CPU::INSTRUCTION* _instruct) noexcept
-{
-  _A = 1;
-
-  while(_A < _instruct->_bytes_to_read)
-  {  
-    read(_PC + _A);    
-    
-    std::cout << _X;
-
-    if(_X == '\n')
-    {
-      break;
-    }
-    ++_A;
-  }
-  _PC += _A + 1;
+  _PC = (_PC | _Y) << 8;
+  _PC = (_PC | _F);
 }
 
 NONE CPU::POP(CPU::INSTRUCTION*) noexcept
@@ -150,7 +192,7 @@ NONE CPU::POP(CPU::INSTRUCTION*) noexcept
   ++_PC;
 }
 
-NONE CPU::PUH(CPU::INSTRUCTION*) noexcept
+NONE CPU::PSH(CPU::INSTRUCTION*) noexcept
 {
   write(_STKPTR, _H);
   

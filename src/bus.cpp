4c59868e70e9d4d607c2ecc0664dc238cedb1ad3
Linuxperoxo <linuxperoxo@gmail.com>
2024-10-21 14:29:53 -0300

    little update

--- src/bus.cpp
+++ src/bus.cpp
@@ -24,18 +24,18 @@ BUS::BUS(CPU* _cpu, RAM* _ram) noexcept
  *
  *  Typedef
  *
- * DATA_BITS_SIZE  -> uint8_t
- * ADDRS_BITS_SIZE -> uint16_t
+ * uint8_t  -> uint8_t
+ * uint16_t -> uint16_t
  * NONE            -> void
  *
  */
 
-uint8_t BUS::read(ADDRS_BITS_SIZE _addrs_to_read) noexcept
+uint8_t BUS::read(uint16_t _addrs_to_read) noexcept
 {
   return _RAM->read(_addrs_to_read);
 }
 
-void BUS::write(ADDRS_BITS_SIZE _addrs_to_write, DATA_BITS_SIZE _data_to_write) noexcept
+void BUS::write(uint16_t _addrs_to_write, uint8_t _data_to_write) noexcept
 {
   _RAM->write(_addrs_to_write, _data_to_write); 
 }
